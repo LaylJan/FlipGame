8eec1766b845cc286dd61c6ec0c7c811e45c834f
Layljan <ljdumayas@gmail.com>
2024-06-01 18:17:58 +0800

    feat: displayed all the cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import back from "./assets/back.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col justify-center items-center min-h-screen p-10">
+      <div className="text-center mb-8">
+        <p>HELLO FLIP</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="flex justify-center grid grid-cols-3 grid-rows-2 w-2/4 ">
+        {[...Array(6)].map((_, index) => (
+          <div key={index} className="flex justify-center items-center">
+            <img src={back} alt="" className="w-36 m-10" />
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
